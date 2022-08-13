4a33c282c2f76f993e2568a29d1b161408c273fd
Alex <alex@q-83.com>
2022-08-13 19:37:41 +1000

    更新启动文件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import "dotenv/config";
 import express from "express";
 import gracefulShutdownHandler from "./utils/gracefulExit";
 import serverRunning from "./utils/severRunning";
 import builderRouters from "./router";
 import cookieSession from "cookie-session";
 import bodyParser from "body-parser";
-import "dotenv/config";
 
 // Create an Express app
 const app = express();
@@ -13,12 +13,23 @@ app.set("trust proxy", true); // 用于解决 nginx 的代理问题
 app.use(
   cookieSession({
     signed: false,
-    secure: true, // cookie session only works in https
+    //secure: true, // cookie session only works in https, 注视掉，方便本地测试
   })
 );
 
 app.use(builderRouters);
 
+// handle ungracefully exit
 const server = app.listen(3000, serverRunning);
-process.on("SIGINT", () => gracefulShutdownHandler(server));
-process.on("SIGTERM", () => gracefulShutdownHandler(server));
+[
+  "exit",
+  "SIGINT",
+  "SIGUSR1",
+  "SIGUSR2",
+  "uncaughtException",
+  "SIGTERM",
+].forEach((eventType) => {
+  process.on(eventType, async () => {
+    await server.close();
+  });
+});
