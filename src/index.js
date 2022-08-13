1519e8d49d316fcf7ea400e04af5e0f61c7f7db1
Alex <alex@q-83.com>
2022-08-13 18:50:02 +1000

    添加了 body parse 来获取 req 中 body 的值

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import gracefulShutdownHandler from "./utils/gracefulExit";
 import serverRunning from "./utils/severRunning";
 import builderRouters from "./router";
 import cookieSession from "cookie-session";
+import bodyParser from "body-parser";
 import "dotenv/config";
 
 // Create an Express app
 const app = express();
+app.use(bodyParser.json());
 app.set("trust proxy", true); // 用于解决 nginx 的代理问题
 app.use(
   cookieSession({
