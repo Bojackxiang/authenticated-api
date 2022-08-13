8841372341cde99e67ff212629a91cbb492dbdb0
Alex <alex@q-83.com>
2022-08-13 18:41:58 +1000

    添加了 cookie session 的插件来处理 request 中的 cookie

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import express from "express";
 import gracefulShutdownHandler from "./utils/gracefulExit";
 import serverRunning from "./utils/severRunning";
 import builderRouters from "./router";
+import cookieSession from "cookie-session";
 import "dotenv/config";
 
 // Create an Express app
 const app = express();
+app.set("trust proxy", true); // 用于解决 nginx 的代理问题
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true, // cookie session only works in https
+  })
+);
 
 app.use(builderRouters);
 
