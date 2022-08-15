51f0cd1e3ea907d6f0e92c8357d58c77e63cb48e
Alex <alex@q-83.com>
2022-08-15 14:34:17 +1000

    重构 index 文件

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import "dotenv/config";
 import app from "./app";
 import serverRunning from "./utils/severRunning";
 
-// handle ungracefully exit
-const server = app.listen(3000, serverRunning);
-[
-  "exit",
-  "SIGINT",
-  "SIGUSR1",
-  "SIGUSR2",
-  "uncaughtException",
-  "SIGTERM",
-].forEach((eventType) => {
-  process.on(eventType, async () => {
-    await server.close();
+const start = () => {
+  // handle ungracefully exit
+  const server = app.listen(3000, serverRunning);
+  [
+    "exit",
+    "SIGINT",
+    "SIGUSR1",
+    "SIGUSR2",
+    "uncaughtException",
+    "SIGTERM",
+  ].forEach((eventType) => {
+    process.on(eventType, async () => {
+      await server.close();
+    });
   });
-});
+};
+
+start();
