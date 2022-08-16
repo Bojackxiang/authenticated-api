2825e155a62094fd43cb49409181098bf786c139
Alex <alex@q-83.com>
2022-08-16 19:46:51 +1000

    finish swagger sign up, sign in, and sign out

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -5,7 +5,7 @@ import config from "../appConfig";
 // use path notmalize 可以找到想要的相对路径，就算是 build 完成了也可以
 const root = path.normalize(`${__dirname}/..`);
 export const docPath = [`${root}/swagger/routers/router-*.yaml`];
-export const schemes = ["https"];
+export const schemes = ["http", "https"];
 const swaggerDefinition = {
   ...config.swagger,
   docPath: docPath,
